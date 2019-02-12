8625cb03332fa14990cd7621c57c636624809999
maroluke <visitme@lukacmarko.com>
2019-02-12 10:34:46 +0100

    add google analytics

--- index.php
+++ index.php
@@ -326,6 +326,16 @@
       	makeRequest(valid);
     }, false);
   </script>
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-134382378-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-134382378-1');
+  </script>
 </body>
 
 </html>
